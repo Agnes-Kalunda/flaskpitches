28a0b0d35c41b55cc0f341ec307c964c030f760a
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-09 02:58:43 +0300

    updated templates

---
+++ app/main/views.py
@@ -0,0 +1,75 @@
+from . import main
+from flask import render_template, request, redirect, url_for, abort
+from flask_login import login_required, current_user
+from ..models import User,Pitch
+from .forms import UpdateProfile, PitchForm
+from .. import db,photos
+
+
+@main.route('/')
+def index():
+    title = "Pitch | Home"
+    return render_template('index.html', title = title)
+@main.route("/new/pitch")
+@login_required
+def new_post():
+    title = "Create Pitch"
+    return render_template("new_pitch.html", title=title)
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html", user=user)
+@main.route('/user/<uname>/update', methods=["GET","POST"])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile', uname=user.username))
+    return render_template('profile/update.html', form=form)
+@main.route('/user/<uname>/update/pic', methods=["POST"])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = 'photos/{}'.format(filename)
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname)) 
+
+
+@main.route('/new/pitch', methods=["GET","POST"])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        pitch = form.pitch.data
+
+        # Update Pitch
+        new_pitch = Pitch(pitch_title=title, pitch_body=pitch, user=current_user)
+
+        # Save Pitch 
+        new_pitch.save_pitch()
+        return redirect(url_for('.user_pitch',uname=current_user))
+    title = "Pitch | New"
+    user = current_user
+    user_details = User.query.filter_by(username=user).first()
+    return render_template('new_pitch.html', title=title, pitch_form=form, user=user_details)
+
+@main.route('/<uname>/pitch>')
+@login_required
+def user_pitch(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template("pitch/user_pitch.html", user=user) 
--- app/models.py
+++ app/models.py
@@ -1,22 +1,38 @@
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
-
-class User(db.Model):
+from flask_login import UserMixin
+from datetime import datetime
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
+    #schema
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password_hash = db.Column(db.String(255))
+    username = db.Column(db.String(255), index=True)
+    password_hash = db.Column(db.String(255), unique=True, index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitch = db.relationship('Pitch', backref="user", lazy="dynamic")
+
+
 
     def __repr__(self):
         return 'User {}'.format(self.username)
-
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute.')
-
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
-
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Pitch(db.Model):
+    __tablename__ = 'pitch_table'
+    id = db.Column(db.Integer, primary_key=True)
+    pitch_title = db.Column(db.String)
+    pitch_body = db.Column(db.String)
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id")
