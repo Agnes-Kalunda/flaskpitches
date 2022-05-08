17200cd5748cba035155472a67ebadca3d5aa70e
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-08 18:30:17 +0300

    defined user data in models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,25 @@
+from ensurepip import bootstrap
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from SQLAlchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy
+
+
 
 def create_app(config_name):
     app = Flask(__name__)
+    
+    #app configurations
+    
+    app.config.from_object(config_options[config_name])
+    
+    #flask extensions
+    
+    bootstrap.init_app(app)
+    db.init_app(app)
+    
+    return app
