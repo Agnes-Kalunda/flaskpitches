06c5259e517b93cec742cd00970aac1fedbee213
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-09 21:13:25 +0300

    debugging

--- app/models.py
+++ app/models.py
@@ -3,11 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +14,9 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    #creating a relationship between user and posts class
     pitch = db.relationship('Pitch', backref="user", lazy="dynamic")
+     #creating a relationship between user and comments
     user_comment = db.relationship('Comment', backref="user", lazy="dynamic")
 
     @property
@@ -27,31 +27,33 @@ class User(UserMixin,db.Model):
         self.password_hash = generate_password_hash(password)
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
-
 class Pitch(UserMixin,db.Model):
     __tablename__ = 'pitch_table'
     id = db.Column(db.Integer, primary_key=True)
     pitch_title = db.Column(db.String)
     pitch_body = db.Column(db.String)
-    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow())
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     category = db.Column(db.String(255))
+    #creating a relationship between post and comment
     pitch = db.relationship("Comment", backref="user_pitch", lazy="dynamic")
     posted_by = db.Column(db.String)
+    #creating a relationship to link a post to a user img
+    # profile_pic_path=db.relationship('User',backref='user', lazy='dynamic')
 
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
-
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    comment = db.Column(db.String)
-    pitch_id = db.Column(db.Integer, db.ForeignKey("pitch_table.id"))
+    comment = db.Column(db.String)  #comment a user gives to a post
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitch_table.id"))  #post id for linking a comment to a specific post
     postedAt = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id")) #user id for linking a comment to a specific user
     posted_by = db.Column(db.String)
+
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
--- config.py
+++ config.py
@@ -1,25 +1,23 @@
 import os
+
+
 class Config:
     """
     General parent configuration parent class
     """
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://daniel:daniel@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-
     @staticmethod
     def init_app(app):
         pass
 class ProdConfig(Config):
-    """
-    """
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")    
 class DevConfig(Config):
-    """
-    """
+    
     DEBUG = True
 config_options = {
     'development': DevConfig,
