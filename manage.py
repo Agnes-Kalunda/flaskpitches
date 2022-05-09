0e744bb6739d9d1f4db1f83f604983e022f11c7f
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-09 13:26:47 +0300

    imported comment and pitch to manage.py

--- manage.py
+++ manage.py
@@ -1,16 +1,25 @@
-from app import create_app, db
-from flask_script import Manager, Server	from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand	from flask_migrate import Migrate, MigrateCommand
-app = create_app('development')	app = create_app('development')
-manager = Manager(app)	manager = Manager(app)
-manager.add_command('server', Server)	manager.add_command('server', Server)
-migrate Migrate(app, db)	migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)	manager.add_command('db', MigrateCommand)
+from app import create_app,db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User, Pitch, Comment
 
 
-@manager.shell	@manager.shell
-def make_shell_context():	def make_shell_context():
-    return dict(app=app)	    return dict(app=app, db=db)
+# app = create_app('development')
+app = create_app('production')
+manager = Manager(app)
+manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run((tests))
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment)
 
 
 
