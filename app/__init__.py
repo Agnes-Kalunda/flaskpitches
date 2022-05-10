6ebc9ad9d28ac6d26d410b5ff849a7da7be930bb
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-10 19:43:46 +0300

    initialized migrations folder

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,21 @@ from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_simplemde import SimpleMDE
 from flask_mail import Mail
+from flask_migrate import Migrate
+
+# from config import app_config
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos', IMAGES)
 simple = SimpleMDE()
 mail=Mail()
+migrate= Migrate()
 login_manager = LoginManager()
+
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
 def create_app(config_name):
     
     
@@ -28,6 +34,9 @@ def create_app(config_name):
     login_manager.init_app(app)
     simple.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app,db)
+    
+    
 
     from .main import main as main_blueprint 
     app.register_blueprint(main_blueprint)
